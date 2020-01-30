d99aaac1a597a311cfac6652ca9e93a447e57f31
thalesu <thalesu@gmail.com>
2020-01-29 23:25:36 -0200

    fix for deploy

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const env = require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors')
-const connect = require('./connection');
+const cors = require('cors');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(routes); 
  
-connect();
+mongoose.connect(process.env.MONGODB_ADDRESS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 3333;
 
 app.listen(port); 
