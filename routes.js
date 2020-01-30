a904ebb950bebe29dc9198315f04269ef7043827
thalesu <thalesu@gmail.com>
2020-01-30 00:28:11 -0200

    update

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const routes = Router();
 
 const UserController = require('./controllers/UserController');
 
+routes.get("/", (req,res) => res.sendStatus(404));
 routes.put("/user", UserController.store);
 routes.delete("/user/:id", UserController.delete);
 routes.get("/users", UserController.index);
